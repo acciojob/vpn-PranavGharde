e8d2dfdacf902e3897c5307903142fb5f59bedf1
PranavGharde <pranavgharde32@gmail.com>
2025-04-06 22:17:08 +0530

    VPN ASSIGNMENT COMPLETED

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,16 +1,12 @@
 package com.driver.test;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(MockitoJUnitRunner.Silent.class)
 public class TestCases {
 
     @Test
-    public void sampleTest() {
-        assertEquals(4, 2 + 2);
+    public void testExample() {
+        assertTrue(true); // Always passes
     }
 }
